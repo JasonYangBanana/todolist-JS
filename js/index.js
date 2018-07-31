f6b3089e5271aec93f6128c07042a39a9c4b8393
JasonYang <jason19901207@gmail.com>
2018-07-31 17:54:51 +0800

    修改命名

--- js/index.js
+++ js/index.js
@@ -1,32 +1,19 @@
 /* 宣告變數區 */
 //新增區
-let addList = document.getElementById('addList');
-let inputText = addList.childNodes[1];
-let addListBtn = addList.childNodes[3];
+let addListItem = document.getElementById('addListItem');
+let inputContent = addListItem.childNodes[1];
+let addListItemBtn = addListItem.childNodes[3];
 //顯示區
-let todoList = document.getElementById('todoList');
-
-// let deleteBtn = document.querySelector(".delete-btn");
-
-// let checkFinish = todoList.childNodes[1].childNodes[1];
-// let listContent = todoList.childNodes[1].childNodes[3];
-// let modifyContent = todoList.childNodes[1].childNodes[5];
-
+let showList = document.getElementById('showList');
 
 /* 宣告函式區 */
-function addTask() {
-    addNewList();
-    // cloneInputText();
-}
-
-// let i = 0;
-function addNewList() {
-    if (inputText.value === "") {
+function addNewTask() {
+    if (inputContent.value === "") {
         alert("你還沒輸入task")
     } else {
-        //製作新的list
-        let listitem = document.createElement("li");
-        let checkFinish = document.createElement("input");
+        //製作新的listItem
+        let listItem = document.createElement("li");
+        let checkFinished = document.createElement("input");
         let listItemContent = document.createElement("input");
         let modifyBtn = document.createElement("button");
         let deleteBtn = document.createElement("button");
@@ -35,24 +22,22 @@ function addNewList() {
 
         // i++;
         // list.classList.add("Task" + i);
-        checkFinish.type = "checkbox";
+        checkFinished.type = "checkbox";
         listItemContent.type = "text";
-        
-        deleteBtn.className = "delete-btn";
         
         modifyBtn.appendChild(modifyText);
         deleteBtn.appendChild(deleteText);
-        listitem.appendChild(checkFinish);
-        listitem.appendChild(listItemContent);
-        listitem.appendChild(modifyBtn);
-        listitem.appendChild(deleteBtn);
-        todoList.appendChild(listitem);
+        listItem.appendChild(checkFinished);
+        listItem.appendChild(listItemContent);
+        listItem.appendChild(modifyBtn);
+        listItem.appendChild(deleteBtn);
+        showList.appendChild(listItem);
 
         /* 將新增區的input值導入顯示區 */
-        listItemContent.value = inputText.value;
+        listItemContent.value = inputContent.value;
 
         /* 清空input的value */
-        inputText.value = "";
+        inputContent.value = "";
         deleteBtn.addEventListener("click", deleteTask, false);
     }
 }
@@ -62,6 +47,6 @@ function deleteTask() {
     var ul = listItem.parentNode;
     ul.removeChild(listItem);
 }
-addListBtn.addEventListener("click", addTask, false);
+addListItemBtn.addEventListener("click", addNewTask, false);
 
 
