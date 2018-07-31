911b16cb3da7aecce43052187f8b46f71d089f09
JasonYang <jason19901207@gmail.com>
2018-07-31 19:00:24 +0800

    完成todo-list

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 /* 宣告變數區 */
-//新增區
-let addListItem = document.getElementById('addListItem');
-let inputContent = addListItem.childNodes[1];
-let addListItemBtn = addListItem.childNodes[3];
-//顯示區
+//視覺-輸入區
+let addTask = document.getElementById('addListItem');
+let inputContent = addTask.childNodes[1];
+let addListItemBtn = addTask.childNodes[3];
+//視覺-顯示區
 let showList = document.getElementById('showList');
 
 /* 宣告函式區 */
@@ -13,10 +13,12 @@ function addNewTask() {
     } else {
         //製作新的listItem
         let listItem = document.createElement("li");
+
         let checkFinished = document.createElement("input");
         let listItemContent = document.createElement("input");
         let modifyBtn = document.createElement("button");
         let deleteBtn = document.createElement("button");
+
         let modifyText = document.createTextNode("Modify");
         let deleteText = document.createTextNode("Delete");
 
@@ -24,7 +26,7 @@ function addNewTask() {
         listItemContent.type = "text";
         listItemContent.disabled = true;
         
-        /* 修改、刪除按鈕建立文字 */
+        /* 修改、刪除按鈕，插入文字 */
         modifyBtn.appendChild(modifyText);
         deleteBtn.appendChild(deleteText);
 
@@ -34,14 +36,15 @@ function addNewTask() {
         listItem.appendChild(deleteBtn);
         showList.appendChild(listItem);
 
-        /* 將新增區的input值導入顯示區 */
+        /* 將user的input值導入顯示區的內文 */
         listItemContent.value = inputContent.value;
 
-        /* 清空input的value */
+        /* 清空user輸入區input的value */
         inputContent.value = "";
+
         console.log("新增Task被執行");
 
-        /* 監聽區 */
+        /* 監聽listItem上的互動 */
         deleteBtn.addEventListener("click", deleteTask, false);
         checkFinished.addEventListener("change", checkTaskFinished, false);
         modifyBtn.addEventListener("click", modifyTask, false);
@@ -57,20 +60,26 @@ function deleteTask() {
 }
 
 function checkTaskFinished() {
-    console.log("完成Task的checkbox被執行");
+    console.log("是否完成Task的checkbox被執行");
 }
+
 function modifyTask() {
     let listItem = this.parentNode;
+    listItem.childNodes[0].disabled = true;
     listItem.childNodes[1].disabled = false;
+    listItem.childNodes[2].disabled = true;
+    listItem.childNodes[3].disabled = true;
     console.log("修改Task被執行");
     listItem.childNodes[1].addEventListener("keydown", confirmModifyContent, false);
 }
 
-/*  */
 function confirmModifyContent(e) {
     if (13 == e.keyCode) {
         let listItem = this.parentNode;
+        listItem.childNodes[0].disabled = false;
         listItem.childNodes[1].disabled = true;
+        listItem.childNodes[2].disabled = false;
+        listItem.childNodes[3].disabled = false;
         console.log("你按了Enter鍵");
     } else {
         console.log("你沒按Enter鍵");
@@ -78,5 +87,4 @@ function confirmModifyContent(e) {
 }
 
 addListItemBtn.addEventListener("click", addNewTask, false);
-
 
